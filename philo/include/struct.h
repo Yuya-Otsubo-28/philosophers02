89ecd32393abdab1cc7d28c42c995519682eb750
yotsubo <513047.otsubo@gmail.com>
2024-09-27 15:15:22 +0900

    modified: struct.h: add num_of_philo to t_data

--- philo/include/struct.h
+++ philo/include/struct.h
@@ -4,17 +4,18 @@
 # include <pthread.h>
 
 typedef struct s_philo {
-	pthread_mutex_t	right;
-	pthread_mutex_t	left;
+	pthread_mutex_t	*right;
+	pthread_mutex_t	*left;
 	int				eat_count;
 }	t_philo ;
 
 typedef struct s_data {
 	t_philo			**philos;
 	pthread_mutex_t	*forks;
+	int				num_of_philo;
+	int				time_for_sleep;
 	int				time_to_death;
 	int				time_for_eat;
-	int				time_for_sleep;
 	int				must_eat;
 }	t_data ;
 
