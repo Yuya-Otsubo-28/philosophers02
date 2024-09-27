f6f114c5d22e483bd169e9bacbc9bcd7516f6e78
yotsubo <513047.otsubo@gmail.com>
2024-09-27 15:01:19 +0900

    modified: struct.h: change forks data type

--- philo/include/struct.h
+++ philo/include/struct.h
@@ -4,18 +4,18 @@
 # include <pthread.h>
 
 typedef struct s_philo {
-	pthread_t	right;
-	pthread_t	left;
-	int			eat_count;
+	pthread_mutex_t	right;
+	pthread_mutex_t	left;
+	int				eat_count;
 }	t_philo ;
 
 typedef struct s_data {
-	t_philo		**philos;
-	pthread_t	*forks;
-	int			time_to_death;
-	int			time_for_eat;
-	int			time_for_sleep;
-	int			must_eat;
+	t_philo			**philos;
+	pthread_mutex_t	*forks;
+	int				time_to_death;
+	int				time_for_eat;
+	int				time_for_sleep;
+	int				must_eat;
 }	t_data ;
 
 #endif
