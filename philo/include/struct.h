7cf418988978151f3861984a6ee023d63ee381f1
yotsubo <513047.otsubo@gmail.com>
2024-09-28 17:30:55 +0900

    modified: struct.h: make fork's struct

--- philo/include/struct.h
+++ philo/include/struct.h
@@ -3,20 +3,25 @@
 
 # include <pthread.h>
 
+typedef struct s_fork {
+	pthread_mutex_t	fork;
+	int				id;
+}	t_fork ;
+
 typedef struct s_philo {
-	pthread_mutex_t	*right;
-	pthread_mutex_t	*left;
-	int				eat_count;
+	t_fork	*right;
+	t_fork	*left;
+	int		eat_count;
 }	t_philo ;
 
 typedef struct s_data {
-	t_philo			**philos;
-	pthread_mutex_t	*forks;
-	int				num_of_philo;
-	int				time_for_sleep;
-	int				time_to_death;
-	int				time_for_eat;
-	int				must_eat;
+	t_philo	**philos;
+	t_fork	**forks;
+	int		num_of_philo;
+	int		time_for_sleep;
+	int		time_to_death;
+	int		time_for_eat;
+	int		must_eat;
 }	t_data ;
 
 #endif
