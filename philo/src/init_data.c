b9588322404ad79eb6b15f881925705b847c9eba
yotsubo <513047.otsubo@gmail.com>
2024-09-29 22:32:26 +0900

    add: init_data.c: init_philo

--- philo/src/init_data.c
+++ philo/src/init_data.c
@@ -7,18 +7,18 @@ t_bool	is_invalid_input(char *nbrp)
 	long long	lln;
 	size_t		i;
 
-	if (!nbpr)
+	if (!nbrp)
 		return (TRUE);
-	if (ft_strlen(nbpr) > INT_MAX_DIGIT)
+	if (ft_strlen(nbrp) > INT_MAX_DIGIT)
 		return (TRUE);
 	lln = 0LL;
 	i = 0;
-	while (nbpr[i])
+	while (nbrp[i])
 	{
-		if (ft_isdigit(nbpr[i]))
+		if (ft_isdigit(nbrp[i]))
 			return (TRUE);
 		lln *= 10;
-		lln += nbpr[i] - '0';
+		lln += nbrp[i] - '0';
 		i++;
 	}
 	if (lln > INT_MAX)
@@ -28,19 +28,36 @@ t_bool	is_invalid_input(char *nbrp)
 
 t_data	*init_num_datas(t_data *data, char **argv)
 {
+	if (is_invalid_input(argv[1]))
+		return (init_error(data));
+	data->num_of_philo = ft_atoi(argv[1]);
 	if (is_invalid_input(argv[2]))
 		return (init_error(data));
-	data->num_of_philo = ft_atoi(argv[2]);
+	data->time_to_die = ft_atoi(argv[2]);
 	if (is_invalid_input(argv[3]))
 		return (init_error(data));
-	data->time_to_die = ft_atoi(argv[3]);
-	if (is_invalid_input(argv[4]))
+	data->time_to_eat = ft_atoi(argv[3]);
+	if (is_invaild_input(argv[4]))
 		return (init_error(data));
-	data->time_to_eat = ft_atoi(argv[4]);
-	if (is_invaild_input(argv[5]))
-		return (init_error(data));
-	data->time_to_sleep = ft_atoi(argv[5]);
+	data->time_to_sleep = ft_atoi(argv[4]);
 	return (data);
+}
+
+t_philo	*init_philo(t_data *data, int i)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)malloc(sizeof(t_philo));
+	if (!philo)
+		return (NULL);
+	memset(philo, 0, sizeof(t_philo));
+	philo->right = data->forks[i];
+	if (i == 0)
+		philo->left = data->forks[data->num_of_philo - 1];
+	else
+		philo->left = data->forks[i - 1];
+	philo->eat_count = 0;
+	return (philo);
 }
 
 t_data *init_philos(t_data *data)
@@ -54,7 +71,7 @@ t_data *init_philos(t_data *data)
 	i = 0;
 	while (i < data->num_of_philo)
 	{
-		data->philos[i] = init_philo();
+		data->philos[i] = init_philo(i);
 		if (!(data->philos[i]))
 			return (init_error(data));
 		i++;
@@ -87,11 +104,21 @@ t_data	*init_forks(t_data *data)
 	i = 0;
 	while (i < data->num_of_philo)
 	{
-		data->forks[i] = init_fork(i);
+		data->forks[i] = init_fork(data, i);
 		if (!(data->forks[i]))
 			return (init_error(data));
 		i++;
 	}
+	return (data);
+}
+
+t_data	*init_must_eat(t_data *data, int argc, char *argv[])
+{
+	if (argc != 6)
+		return (data);
+	if (is_invalid_input(argv[5]))
+		return (init_error(data));
+	data->must_eat = ft_itoa(argv[5]);
 	return (data);
 }
 
