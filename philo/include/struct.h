cda52009896410957bd56fb817cf257d3847ac8f
yotsubo <513047.otsubo@gmail.com>
2024-10-03 14:41:44 +0900

    modified: include/: add member to structs

--- philo/include/struct.h
+++ philo/include/struct.h
@@ -10,30 +10,38 @@ typedef struct s_fork {
 	size_t			id;
 }	t_fork ;
 
-// TODO: philoの中にdataを持たせる。
 // TODO: philoの中にmsg_mtxを持たせる。
 // TODO: philoの中にlast_eatを持たせる。
 // TODO: philoの中にtime_to_sleepなど不変でよく参照する変数を追加する。
 typedef struct s_philo {
+	t_data			*data;
 	size_t			id;
 	t_fork			*right;
 	t_fork			*left;
-	pthread_mutex_t	*data_mtx;
+	pthread_mutex_t	*count_mtx;
+	pthread_mutex_t	*msg_mtx;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
 	int				eat_count;
+	int				last_eat;
 }	t_philo ;
 
 // TODO: pthread_mutex_tをラップした構造体を定義し、mutexが初期化されたものかどうかを判定できるようにする。
+// TODO: is_start, is_finishをdataに含める。
+// TODO: count_mtxはpthread_mutex_t配列にする。
 typedef struct s_data {
 	t_philo			**philos;
 	t_fork			**forks;
 	pthread_t		*threads;
 	pthread_t		monitor;
-	pthread_mutex_t	data_mtx;
+	pthread_mutex_t	count_mtx;
+	pthread_mutex_t	msg_mtx;
 	int				num_of_philo;
-	int				time_to_sleep;
+	int				must_eat;
 	int				time_to_die;
 	int				time_to_eat;
-	int				must_eat;
+	int				time_to_sleep;
 }	t_data ;
 
 #endif
