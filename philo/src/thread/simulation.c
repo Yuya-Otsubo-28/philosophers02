11fa728090aa2df90e17938b3e0bfc12ad103bfa
yuotsubo <513047.otsubo@gmail.com>
2024-12-20 00:40:18 +0900

    [#24]ref: simulation.c: adjust to norm

--- philo/src/thread/simulation.c
+++ philo/src/thread/simulation.c
@@ -6,11 +6,20 @@
 /*   By: yotsubo <y.otsubo.886@ms.saitama-u.ac.j    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 13:53:14 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/12/18 15:10:33 by yotsubo          ###   ########.fr       */
+/*   Updated: 2024/12/20 00:37:34 by yotsubo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+static t_bool	drop_forks(t_fork *first, t_fork *second)
+{
+	if (first)
+		pthread_mutex_unlock(&(first->my_mtx->mtx));
+	if (second)
+		pthread_mutex_unlock(&(second->my_mtx->mtx));
+	return (TRUE);
+}
 
 t_bool	is_finish(t_philo *philo)
 {
@@ -29,27 +38,15 @@ static void	even_simulation(t_philo *philo)
 	while (1)
 	{
 		taking_fork(philo, RIGHT);
-		if (is_finish(philo))
-		{
-			pthread_mutex_unlock(&(philo->right->my_mtx->mtx));
+		if (is_finish(philo) && drop_forks(philo->right, NULL))
 			break ;
-		}
 		taking_fork(philo, LEFT);
-		if (is_finish(philo))
-		{
-			pthread_mutex_unlock(&(philo->right->my_mtx->mtx));
-			pthread_mutex_unlock(&(philo->left->my_mtx->mtx));
+		if (is_finish(philo) && drop_forks(philo->left, philo->right))
 			break ;
-		}
 		eating(philo);
-		if (is_finish(philo))
-		{
-			pthread_mutex_unlock(&(philo->right->my_mtx->mtx));
-			pthread_mutex_unlock(&(philo->left->my_mtx->mtx));
+		if (is_finish(philo) && drop_forks(philo->left, philo->right))
 			break ;
-		}
-		pthread_mutex_unlock(&(philo->left->my_mtx->mtx));
-		pthread_mutex_unlock(&(philo->right->my_mtx->mtx));
+		drop_forks(philo->left, philo->right);
 		if (is_finish(philo))
 			break ;
 		sleeping(philo);
@@ -69,27 +66,15 @@ static void	odd_simulation(t_philo *philo)
 		if (is_finish(philo))
 			break ;
 		taking_fork(philo, LEFT);
-		if (is_finish(philo))
-		{
-			pthread_mutex_unlock(&(philo->left->my_mtx->mtx));
+		if (is_finish(philo) && drop_forks(philo->left, NULL))
 			break ;
-		}
 		taking_fork(philo, RIGHT);
-		if (is_finish(philo))
-		{
-			pthread_mutex_unlock(&(philo->left->my_mtx->mtx));
-			pthread_mutex_unlock(&(philo->right->my_mtx->mtx));
+		if (is_finish(philo) && drop_forks(philo->right, philo->left))
 			break ;
-		}
 		eating(philo);
-		if (is_finish(philo))
-		{
-			pthread_mutex_unlock(&(philo->left->my_mtx->mtx));
-			pthread_mutex_unlock(&(philo->right->my_mtx->mtx));
+		if (is_finish(philo) && drop_forks(philo->right, philo->left))
 			break ;
-		}
-		pthread_mutex_unlock(&(philo->right->my_mtx->mtx));
-		pthread_mutex_unlock(&(philo->left->my_mtx->mtx));
+		drop_forks(philo->right, philo->left);
 		if (is_finish(philo))
 			break ;
 		sleeping(philo);
